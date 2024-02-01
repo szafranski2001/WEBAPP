005583c40ab71df26d1118f7b8397f25c352d5b2
Davide <petittodavide31@gmail.com>
2024-02-01 17:55:02 +0100

    Drafts of header and footer added

--- TeatroCosenza/src/app/app.module.ts
+++ TeatroCosenza/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
