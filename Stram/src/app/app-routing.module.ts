9a8c6b4b43addd754f5c53b643b92229ff2a2241
MonettaChristianFrancesco <mntcrs02l08g975q@studenti.unical.it>
2024-06-28 11:40:16 +0200

    adding guard to routes

--- Stram/src/app/app-routing.module.ts
+++ Stram/src/app/app-routing.module.ts
@@ -10,18 +10,19 @@ import { NotfoundComponent } from './Components/notfound/notfound.component';
 import {SignupComponent} from "./Components/signup/signup.component";
 import {AddGameComponent} from "./Components/add-game/add-game.component";
 import { AdminReportsComponent } from './Components/admin-reports/admin-reports.component';
+import { adminGuard, authGuard } from './services/auth.guard';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'addGame', component: AddGameComponent},
-  {path: 'profile', component:ProfileComponent},
+  {path: 'addGame', component: AddGameComponent},//inserire guard quando si completa la pagina
+  {path: 'profile', component:ProfileComponent,canActivate:[authGuard]},
   {path: 'signup', component: SignupComponent},
   {path: 'login', component: LoginComponent},
   {path: 'guidelines', component:GuidelinesComponent},
   {path: 'videogame/:id', component:VideogameDetailComponent},
-  {path: 'videogame-lists', component: VideogameListsComponent},
+  {path: 'videogame-lists', component: VideogameListsComponent},//inserire guard quando si completa la pagina
+  {path: 'reports-handling', component: AdminReportsComponent,canActivate:[authGuard,adminGuard]},
   {path: '404', component: NotfoundComponent},
-  {path: 'reports-handling', component: AdminReportsComponent},
   {path: '**', redirectTo: '/404'}
 ];
 
---
+++ Stram/src/app/services/auth.guard.ts
@@ -0,0 +1,23 @@
+import { Inject, inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+
+export const authGuard: CanActivateFn = (route, state) => {
+
+  const router : Router = inject(Router);
+
+  if(localStorage.getItem("user") != null)
+    return true;
+  else
+    return router.navigate(["/"]);
+};
+
+export const adminGuard : CanActivateFn = (route,state) => {
+
+  const router : Router = inject(Router);
+  
+  if(Number(localStorage.getItem("type")) == 1)
+    return true;
+  else
+    return router.navigate(["/"]);
+  
+};
