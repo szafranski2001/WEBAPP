1760ff22e91aeffb881e2d477e18d3a1514e0c9a
mario-bruno <106055413+mario-bruno@users.noreply.github.com>
2024-06-30 16:18:28 +0200

    db top10/get10

---
+++ StramBackEnd/src/main/java/it/unical/studenti/strambackend/commands/concreteCommands/home/GetSliders.java
@@ -0,0 +1,36 @@
+package it.unical.studenti.strambackend.commands.concreteCommands.home;
+
+import it.unical.studenti.strambackend.commands.Command;
+import it.unical.studenti.strambackend.persistence.DBManager;
+import it.unical.studenti.strambackend.persistence.Model.Videogioco;
+import it.unical.studenti.strambackend.util.Settings;
+import org.springframework.http.ResponseEntity;
+
+import java.util.*;
+
+public class GetSliders extends Command {
+    public GetSliders(Map<String, String> allParameters) {
+        super(allParameters);
+    }
+
+    @Override
+    protected <T> ResponseEntity<T> logicCommand(Map<String, String> allParameters) {
+        Map<String, List<Videogioco>> res = new HashMap<>();
+
+        res.put("big-poster", null);
+        res.put("Top 10", DBManager.getInstance().VideogiocoDAO());
+        res.put("Adventure", DBManager.getInstance().VideogiocoDAO().risultati(""));
+
+        //DBManager.getInstance().VideogiocoDAO().risultati("")
+
+        return (ResponseEntity<T>) res;
+    }
+
+    @Override
+    protected Set<Settings.params> getRequiredParams() {
+        return Set.of();
+    }
+
+
+
+}
