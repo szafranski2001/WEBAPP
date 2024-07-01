5a4d63288e39b4d1226f202d519a33ca9cefb97e
klaudiusz damian <szfkds01l31z127i@studenti.unical.it>
2024-07-01 19:13:13 +0200

    Guard Login

--- Stram/src/app/services/auth.guard.ts
+++ Stram/src/app/services/auth.guard.ts
@@ -14,10 +14,20 @@ export const authGuard: CanActivateFn = (route, state) => {
 export const adminGuard : CanActivateFn = (route,state) => {
 
   const router : Router = inject(Router);
-  
+
   if(Number(localStorage.getItem("type")) == 1)
     return true;
   else
     return router.navigate(["/"]);
-  
+
+};
+
+export const NoAuthGuard : CanActivateFn = (route,state) => {
+
+  const router : Router = inject(Router);
+
+  if(localStorage.getItem("user") == null)
+    return true;
+  else
+    return router.navigate(["/"]);
 };
