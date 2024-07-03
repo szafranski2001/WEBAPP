aa7e2dcdfc94f1bf21a12d8c1e08c6c8b6c11007
MonettaChristianFrancesco <mntcrs02l08g975q@studenti.unical.it>
2024-07-03 10:14:15 +0200

    deleting useless files and adding guard forgotten on addGame

--- StramBackEnd/src/main/java/it/unical/studenti/strambackend/persistence/exceptions/NoAuthException.java
+++
@@ -1,8 +0,0 @@
-package it.unical.studenti.strambackend.persistence.exceptions;
-
-public class NoAuthException extends Exception{
-
-    public NoAuthException(String message){
-        super(message);
-    }
-}
